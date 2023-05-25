8c7d69e5f972140db9fbcebcf904813b3fe2e032
Sachinkashyap <133212338+sk-kashyap@users.noreply.github.com>
2023-05-25 13:04:55 +0530

    Create Fiservapi.php

---
+++ Fiservapi.php
@@ -0,0 +1,77 @@
+Array
+(
+    [clientRequestId] => e85d2a92-13a3-439a-8fbc-3b414db146c2
+    [apiTraceId] => ZGW72UBjbu4dACxVfg54QwAAAyo
+    [ipgTransactionId] => 84626529243
+    [orderId] => R-25433a18-e6c2-45b3-a8f9-881840546a9f
+    [transactionType] => SALE
+    [paymentToken] => Array
+        (
+            [reusable] => 1
+            [declineDuplicates] => 
+            [brand] => VISA
+            [type] => PAYMENT_CARD
+        )
+
+    [transactionOrigin] => ECOM
+    [paymentMethodDetails] => Array
+        (
+            [paymentCard] => Array
+                (
+                    [expiryDate] => Array
+                        (
+                            [month] => 07
+                            [year] => 2024
+                        )
+
+                    [bin] => 476173
+                    [last4] => 0010
+                    [brand] => VISA
+                )
+
+            [paymentMethodType] => PAYMENT_CARD
+            [paymentMethodBrand] => VISA
+        )
+
+    [country] => Belgium
+    [terminalId] => RC103954
+    [merchantId] => 881111110000228
+    [transactionTime] => 1684388825
+    [approvedAmount] => Array
+        (
+            [total] => 4920
+            [currency] => EUR
+            [components] => Array
+                (
+                    [subtotal] => 4920
+                )
+
+        )
+
+    [transactionAmount] => Array
+        (
+            [total] => 4920
+            [currency] => EUR
+            [components] => Array
+                (
+                    [subtotal] => 4920
+                )
+
+        )
+
+    [transactionStatus] => APPROVED
+    [approvalCode] => Y:726269:4626529243:PPX :718730
+    [schemeTransactionId] => 444132818210647
+    [processor] => Array
+        (
+            [referenceNumber] => 084626529243
+            [authorizationCode] => 726269
+            [responseCode] => 00
+            [responseMessage] => Function performed error-free
+            [avsResponse] => Array
+                (
+                    [streetMatch] => NO_INPUT_DATA
+                    [postalCodeMatch] => NO_INPUT_DATA
+                )
+
+        )
